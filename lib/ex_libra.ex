9c0c4d9d2e380d5a513dd9dd6d2c4c11ce99e171
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-14 01:23:41 -0600

    Listen when my compiler complains

--- lib/ex_libra.ex
+++ lib/ex_libra.ex
@@ -8,10 +8,17 @@ defmodule ExLibra do
   """
   def avatar(email, opts \\ []) do
     email_domain = String.split(email, "@") |> Enum.at(1)
+
     email_hash =
       :crypto.hash(:md5, String.downcase(email))
+      |> Base.encode16(case: :lower)
 
-    answers = DNS.query("_avatars-sec._tcp.#{email_domain}", :srv).anlist
+    answers =
+      try do
+        DNS.query("_avatars-sec._tcp.#{email_domain}", :srv).anlist
+      rescue
+        _ -> []
+      end
 
     avatar_host =
       if Enum.empty?(answers) do
@@ -23,13 +30,14 @@ defmodule ExLibra do
     query_params =
       Keyword.take(opts, [:size, :default])
       |> rename_keys(%{size: "s", default: "d", force_default: "f"})
+      |> URI.encode_query()
 
-    "https://${avatar_host}/avatar/${email_hash}"
+    "https://#{avatar_host}/avatar/#{email_hash}?#{query_params}"
   end
 
   defp rename_keys(map, names) do
     Map.new(map, fn {old_key, val} ->
-      {Map.get(renames, old_key, old_key), val}
+      {Map.get(names, old_key, old_key), val}
     end)
   end
 end
