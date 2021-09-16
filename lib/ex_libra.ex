9dc57f0fefe53e3d069cb793a60d421670045e9f
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-15 20:23:06 -0600

    Add scheme option, document options

--- lib/ex_libra.ex
+++ lib/ex_libra.ex
@@ -5,8 +5,16 @@ defmodule ExLibra do
 
   @doc """
   Fetch the avatar URL for an email address.
+
+  ## Options
+
+    * `:size` - size of the resulting image, between 1 and 512
+    * `:default` - URL to a default image to use, or a special value for an auto-generated avatar
+    * `:force_default` - force the default image to be displayed, even if an avatar exists for this email
+    * `:scheme` - `:http` or `:https`, default is `:https`
   """
   def avatar(email, opts \\ []) do
+    scheme = Keyword.get(opts, :scheme, :https)
     email_domain = String.split(email, "@") |> Enum.at(1)
 
     email_hash =
@@ -15,7 +23,10 @@ defmodule ExLibra do
 
     answers =
       try do
-        DNS.query("_avatars-sec._tcp.#{email_domain}", :srv).anlist
+        case scheme do
+          :https -> DNS.query("_avatars-sec._tcp.#{email_domain}", :srv).anlist
+          :http -> DNS.query("_avatars._tcp.#{email_domain}", :srv).anlist
+        end
       rescue
         _ -> []
       end
@@ -32,7 +43,7 @@ defmodule ExLibra do
       |> rename_keys(%{size: "s", default: "d", force_default: "f"})
       |> URI.encode_query()
 
-    "https://#{avatar_host}/avatar/#{email_hash}?#{query_params}"
+    "#{Atom.to_string(scheme)}://#{avatar_host}/avatar/#{email_hash}?#{query_params}"
   end
 
   defp rename_keys(map, names) do
